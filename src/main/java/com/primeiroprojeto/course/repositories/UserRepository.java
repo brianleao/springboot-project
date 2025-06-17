a03f671d02431aebfe4086f869579b1e1e7d4a44
Brian Leao <brianleao@icloud.com>
2025-06-17 12:00:19 -0300

    feat: TestConfig and UserRepository for users insert added

---
+++ src/main/java/com/primeiroprojeto/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.primeiroprojeto.course.repositories;
+
+import com.primeiroprojeto.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
