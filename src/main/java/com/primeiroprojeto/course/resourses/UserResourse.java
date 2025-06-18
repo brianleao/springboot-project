434e2418f48562571233d62e53cd221c8c6a07ee
Brian Leao <brianleao@icloud.com>
2025-06-18 09:55:43 -0300

    feat: add findAll and findById

--- src/main/java/com/primeiroprojeto/course/resourses/UserResourse.java
+++ src/main/java/com/primeiroprojeto/course/resourses/UserResourse.java
@@ -1,18 +1,35 @@
 package com.primeiroprojeto.course.resourses;
 
+import com.primeiroprojeto.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.primeiroprojeto.course.entities.User;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResourse {
 
+    //para o spring fazer a injeção de dependencia automaticamente
+    @Autowired
+    //Dependencia
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User u = new User(1L, "Brian", "brianleao@icloud,com", "41997447795","123456");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll() {
+
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
