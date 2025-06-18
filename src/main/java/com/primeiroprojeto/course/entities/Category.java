73853741a29ae7bfc7ffd1d943156407df7e45c2
Brian Leao <brianleao@icloud.com>
2025-06-18 12:11:39 -0300

    feat: Category and Product associeted

--- src/main/java/com/primeiroprojeto/course/entities/Category.java
+++ src/main/java/com/primeiroprojeto/course/entities/Category.java
@@ -1,5 +1,6 @@
 package com.primeiroprojeto.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -19,7 +20,8 @@ public class Category implements Serializable {
 
     private String name;
 
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
